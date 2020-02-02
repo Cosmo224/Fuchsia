3a2431ed7a98a2c40656681e14f8e09dd61dbd4f
Cosmo224 <mario64crashed@gmail.com>
2020-02-02 19:32:32 +0000

    FUXDoclink

---
+++ FuchsiaApplicationEngine/FuchsiaUX/FUXDoclink.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Fuchsia.InformationEngine.FuchsiaUX
+{
+    /// <summary>
+    /// Interaction logic for FUXDoclink.xaml
+    /// </summary>
+    public partial class FUXDoclink : UserControl
+    {
+        public FUXDoclink()
+        {
+            InitializeComponent();
+        }
+    }
+}
