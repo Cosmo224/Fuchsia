d64f802780ddc697a0d8b4c6d95be4ffcfa5f1a9
Cosmo224 <mario64crashed@gmail.com>
2020-02-02 19:20:48 +0000

    Ok apis

---
+++ FuchsiaApplicationEngine/FDocument/FDocument.API.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fuchsia.InformationEngine
+{
+    public partial class FInfoEngine
+    {
+        public void LoadDocument(IFDocument DocumentToLoad)
+        {
+            DocumentToLoad.DocumentXML = FLoadXml(DocumentToLoad.DocumentPath);
+            FParseDocument(DocumentToLoad);
+        }
+        public void LoadDocumentByName(IFApp AppToSearchForDocumentIn, string DocumentName)
+        {
+            foreach (IFDocument FDocumentToSearch in AppToSearchForDocumentIn.Documents)
+            {
+                if (FDocumentToSearch.DocumentName == DocumentName)
+                {
+                    
+                    LoadDocument(FDocumentToSearch);
+                }
+            }
+        }
+    }
+}
