6818fee90bedf804cbfafd3fd14030a42e445d11
Cosmo224 <mario64crashed@gmail.com>
2020-02-06 17:52:47 +0000

    Allow Merging

---
+++ FuchsiaApplicationEngine/FuchsiaUX/FUXDoclink.xaml.cs
@@ -0,0 +1,42 @@
+﻿using Fuchsia.InformationEngine;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Fuchsia.InformationEngine.FuchsiaUX
+{
+    /// <summary>
+    /// Interaction logic for FUXDoclink.xaml
+    /// </summary>
+    public partial class FUXDoclink : UserControl
+    {
+        public string Destination { get; set; }
+        public string DocLinkContent { get => FuchsiaUXInternal_FUXDoclink_FUXDoclinkText.Text; set => FuchsiaUXInternal_FUXDoclink_FUXDoclinkText.Text = value; }
+        public FUXDoclink()
+        {
+            InitializeComponent();
+        }
+
+        private void FuchsiaUXInternal_FUXDoclink_FUXDoclinkText_PreviewMouseDown(object sender, MouseButtonEventArgs e)
+        {
+            //handle click 
+            //TEMP?
+            FInfoEngine FIE = new FInfoEngine();
+
+            IFDocument FDocumentToLoad = new FDocument();
+            FDocumentToLoad.DocumentPath = this.Destination;
+            FIE.FLoadDocument(FDocumentToLoad);
+        }
+    }
+}
--- FuchsiaApplicationEngine/Properties/AssemblyInfo.cs
+++ FuchsiaApplicationEngine/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Fuchsia Information Engine")]
+[assembly: AssemblyTitle("Fuchsia Information Engine Alpha SDK")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Fuchsia Information Engine")]
+[assembly: AssemblyProduct("Fuchsia Information Engine Alpha SDK")]
 [assembly: AssemblyCopyright("Copyright © Cosmo 2020")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.66.0")]
-[assembly: AssemblyFileVersion("1.0.66.0")]
+[assembly: AssemblyVersion("1.0.125.0")]
+[assembly: AssemblyFileVersion("1.0.125.0")]
